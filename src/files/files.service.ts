880c7227758791fb9a8d594649f2fe2491cc5ba0
Udhbhav <21889udbhav@gmail.com>
2024-07-25 01:04:06 +0530

    Create upload service in file service

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,6 +1,20 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FilesService {
-    
+    private readonly s3Client = new S3Client({ region: this.configService.get('AWS_REGION') });
+
+    constructor(private readonly configService: ConfigService) {}
+
+    async upload(fileName: string, file: Buffer) {
+        await this.s3Client.send(
+            new PutObjectCommand({
+                Bucket: this.configService.get('AWS_BUCKET_NAME'),
+                Key: fileName,
+                Body: file,
+            }),
+        );
+    }
 }
